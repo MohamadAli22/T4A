dcbbe27606cbd057607e4470339a5a691d64ae3a
MohamadAli <mohamadali.gharat@gmail.com>
2021-02-16 18:56:06 +0330

    Update sendMsg4SlmYar.js

--- sendMsg4SlmYar.js
+++ sendMsg4SlmYar.js
@@ -8,10 +8,7 @@ function textAreaBtnClickFunction() {
 	let hashId = 
 	(document.querySelector("body > div.container.actions > div > div:nth-child(1) > a").href).substr(34,(document.querySelector("body > div.container.actions > div > div:nth-child(1) > a").href.length));
 
-	let authToken = 
-	"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImNjOTI3NDFiMzE5NWFmMTAzMWUwYzM1ODYxMDM2NTMyNTA4NzkzNzgyMGI0YjliNjM2M2M5YjhiODgyZTA3Njk4ODM5MWFjMDA2NjJmZGRhIn0.eyJhdWQiOiIyIiwianRpIjoiY2M5Mjc0MWIzMTk1YWYxMDMxZTBjMzU4NjEwMzY1MzI1MDg3OTM3ODIwYjRiOWI2MzYzYzliOGI4ODJlMDc2OTg4MzkxYWMwMDY2MmZkZGEiLCJpYXQiOjE2MTAwMTIwNzQsIm5iZiI6MTYxMDAxMjA3NCwiZXhwIjoxNjE3Nzg0NDc0LCJzdWIiOiI1OTg1MjciLCJzY29wZXMiOltdfQ.g6UzYop1CZcDMsW0q5zScOJiRzp3u8XzjuvlwCoFj-qSG7T2GzOpiE9tyCT6sl3Os0SwYu_p23WL3F7IRebtAPOuoelItJ0SYfBRpLUwXazwJpcrU6c5eyV5X50KQbRfdvvlQGRoIYzUbZarDUV3VxDWmX17pY3MROJ2KPApn5yJvz0jQsF_dZ8tqrfScmZXoPE-Jyar4fSYiRdeao7yNfgxjLNmE8ZW0PACbPD6EL1luFryy4TabMZaugaBkkc_pgq4X9iN0VlfE2PZFywzwz8Mh7IAtBN26m3U2asgEBypZKtdngl32F5mWpfyB-ZzqE_9a5f9ObAYJML3_WyDiFXu5ctqNhxsuB4Yy_uGb-3uUkwi5ARreIQKNbUM3b9u6vlsmhIXQ8EqeQDapEqvbFd6_Uz1N_kZ_CAYWjjSI6byxzflzD474Gq1NKLi0wvrOsuI-g7eh2QG6TqRq3mPBrhUBJQxy6jf3kS62a4s5He9SVqsGh0caLXDYEt6FcrVOTZBcUXWfaG8sIqOIdrD1320Wwze5tTFSUIPUFTXy15toz9u2QD0JKA7WF7eWIblN2CsX_5Zt71BeZXXW29pFIXZJyFImdvHfqaOWcPZn10dvM3wpcbg2pnjijASSFMqgINzi6zqxl-z_0DHLdvSvK--Aic0e7tAMYNiGGbvWfw";
-
-	console.log(authToken);
+	let authToken = "Bearer "+ readCookie('accessToken');
 
 	$.ajax({
 		type: 'POST',
