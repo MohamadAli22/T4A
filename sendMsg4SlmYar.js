3e95050b9faf7f21cfad93988ede32e1a0bb803f
MohamadAli <mohamadali.gharat@gmail.com>
2021-04-24 12:59:43 +0430

    Update sendMsg4SlmYar.js

--- sendMsg4SlmYar.js
+++ sendMsg4SlmYar.js
@@ -154,7 +154,7 @@ $(document).ready(function () {
     });
   } else {
     $('#txtAreaMSG').val(
-      "لینک خرید *رایگان* آموزش غرفه داری و فروش از سلامیاران عضو باسلام: https://karbalad.basalam.com/blog/salamyar-landing"
+      "لینک خرید آموزش غرفه داری و فروش از سلامیاران عضو باسلام: https://karbalad.basalam.com/blog/salamyar-landing"
     );
   }
 });
